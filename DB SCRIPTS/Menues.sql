2dda083b91e04a7d3d247f9e920e5a93c0b6fc1c
Ran <ranraphaelnir@gmail.com>
2018-06-02 10:53:21 +0300

    clean up db scripts

--- DB SCRIPTS/Menues.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO Menues(PickRate) values(0);
-INSERT INTO Menues(PickRate) values(0);
-INSERT INTO Menues(PickRate) values(0);
-INSERT INTO Menues(PickRate) values(0);
-INSERT INTO Menues(PickRate) values(0);
